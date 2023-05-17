09574ab3a22219c52d326736dc7b9bd0a280d9e0
PravinGhimire <ghimireprabin62@gmail.com>
2023-05-17 11:33:41 +0545

    tanab'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,24 +21,24 @@
 
 <body class="font-sans antialiased">
     <div class="flex ">
-        <div class="w-56 bg-blue-200 shadow-md h-100%">
+        <div class="w-56 bg-white shadow-md h-100%">
             <div class="my-1">
-                <img src="images/logo3.png " class="w-10/12 h-auto bg-white m-auto mx-auto  rounded-3xl radius-0" alt="">
+                <!-- <img src="images/logo3.png " class="w-10/12 h-auto bg-white m-auto mx-auto  rounded-3xl radius-0" alt=""> -->
             </div>
             <div class="mt-13 ">
                 <p class="text-center font-bold text-lg">Hello, {{auth()->user()->name}}</p>
-                <a href="{{route('dashboard')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white  @if(request()->routeIs('dashboard')) bg-blue-600 text-white @endif">Dashboard</a>
+                <a href="{{route('dashboard')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white  @if(request()->routeIs('dashboard')) bg-blue-600 text-white @endif">Dashboard</a>
                 @if(auth()->user()->role=='admin')
-                <a href="{{route('rooms.index')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('rooms.*')) bg-blue-600 text-white @endif">Rooms</a>
-                <a href="{{route('slider.index')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('slider.*')) bg-blue-600 text-white @endif">Sliders</a>
-                <a href="{{route('gallery.index')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white @if(request()->routeIs('gallery.*')) bg-blue-600 text-white @endif">Gallery</a>
-                <a href="{{route('user.index')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('user.*')) bg-blue-600 text-white @endif">Users</a>
-                <a href="{{route('roomdetails.index')}}" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('roomdetails.*')) bg-blue-600 text-white @endif">Room Deatils</a>
-                <a href="" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('Aboutus page.*')) bg-blue-600 text-white @endif">AboutUs Page</a>
+                <a href="{{route('rooms.index')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('rooms.*')) bg-blue-600 text-white @endif">Rooms</a>
+                <a href="{{route('slider.index')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('slider.*')) bg-blue-600 text-white @endif">Sliders</a>
+                <a href="{{route('gallery.index')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white @if(request()->routeIs('gallery.*')) bg-blue-600 text-white @endif">Gallery</a>
+                <a href="{{route('user.index')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('user.*')) bg-blue-600 text-white @endif">Users</a>
+                <a href="{{route('roomdetails.index')}}" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('roomdetails.*')) bg-blue-600 text-white @endif">Room Deatils</a>
+                <a href="" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600 ease-in-out duration-300  hover:text-white @if(request()->routeIs('Aboutus page.*')) bg-blue-600 text-white @endif">AboutUs Page</a>
                 @endif
-                <a href="{{route('booking.index')}}" class="  block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300 @if(request()->routeIs('booking.*')) bg-blue-600 text-white @endif">Booking</a>
-                <a href="/" class="  block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300 @if(request()->routeIs('viewsite.*')) bg-blue-600 text-white @endif">View Site</a>
-                <form action="{{route('logout')}}" method="POST" class="block pl-4 border-b-2 border-green-500 ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300">
+                <a href="{{route('booking.index')}}" class="  block pl-4  ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300 @if(request()->routeIs('booking.*')) bg-blue-600 text-white @endif">Booking</a>
+                <a href="/" class="  block pl-4  ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300 @if(request()->routeIs('viewsite.*')) bg-blue-600 text-white @endif">View Site</a>
+                <form action="{{route('logout')}}" method="POST" class="block pl-4  ml-2 text-xl py-2 hover:bg-violet-600  hover:text-white ease-in-out duration-300">
                     @csrf
                     <button type="submit">Logout</button>
                 </form>
--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @include('layouts.message')
 
-<h2 class="text-4xl font-bold border-b-4 text-red-500 border-blue-500">Rooms</h2>
+<h2 class="text-4xl font-bold text-red-500 ">Rooms</h2>
 <div class="my-4 text-right">
     <a href="{{route('rooms.create')}}" class="bg-green-500 text-white rounded-lg px-3 py-2">Add Room</a>
 

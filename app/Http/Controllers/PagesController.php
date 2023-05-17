ea6ddb78be7fe463117fe494fce82023ae810fa2
PravinGhimire <ghimireprabin62@gmail.com>
2023-05-17 08:25:20 +0545

    hyaa

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use App\Models\Gallery;
 use App\Models\Roomdetails;
 use App\Models\Rooms;
@@ -10,32 +11,41 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function index(){
-        $rooms=Rooms::all();
-        $galleries=Gallery::all();
-        $sliders=Slider::all();
-        return view ('welcome',compact('rooms','galleries','sliders'));
+    public function index()
+    {
+        $rooms = Rooms::all();
+        $galleries = Gallery::all();
+        $sliders = Slider::all();
+        return view('welcome', compact('rooms', 'galleries', 'sliders'));
     }
-    public function about(){
+    public function about()
+    {
         return view('about');
     }
-    public function room(){
-        $rooms=Rooms::all();
+    public function room()
+    {
+        $rooms = Rooms::all();
         return view('room', compact('rooms'));
     }
-    public function gallerys(){
-        $galleries=Gallery::all();
-        return view('gallerys',compact('galleries'));
+    public function gallerys()
+    {
+        $galleries = Gallery::all();
+        return view('gallerys', compact('galleries'));
     }
-    public function header(){
-        return view('header');
+    public function header()
+    {
+        $rooms = Rooms::all();
+        $forms = Booking::with('room')->get();
+        return view('header', compact('rooms', 'forms'));
     }
-    public function footer(){
+    public function footer()
+    {
         return view('footer');
     }
-    public function rums($id){
-        $room=Rooms::find($id);
-        $roomdetail=Roomdetails::where('room_id',$id)->first();
-        return view('rums',compact('room','roomdetail'));
+    public function rums($id)
+    {
+        $room = Rooms::find($id);
+        $roomdetail = Roomdetails::where('room_id', $id)->first();
+        return view('rums', compact('room', 'roomdetail'));
     }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,44 +1,85 @@
 <header>
- <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-  <div class="container ">
-    <a class="navbar-brand" href="/">New Era</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active " href="/about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/room">Our Room</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/gallerys">Gallery</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+    <div class="container ">
+      <a class="navbar-brand" href="/">New Era</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active " href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="/room">Our Room</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="/gallerys">Gallery</a>
+          </li>
+        </ul>
         <div class=" btn-group ">
-        @if(auth()->check())
-        <p class="text-white text-xl m-2 ">Welcome,{{auth()->user()->name}} aalu</p>
-        @endif
-                 <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class='	far fa-user-circle' style='font-size:30px margin-right=1px' href="#"></i>
-               </button>
-                  <ul class="dropdown-menu dropdown-menu-end">
-                     @if(!auth()->check())
-                     <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
-                     @else
-                     <!-- <li><a class="dropdown-item" href="{{route('login')}}">{{auth()->user()->name}}</a></li> -->
-                     <li><a class="dropdown-item" href="{{route('booking.index')}}">View Bookings</a></li>
-                     <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
-                     @endif
-                     <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                  </ul>
-             </div>
+          @if(auth()->check())
+          <p class="text-white text-xl m-2 ">Welcome,{{auth()->user()->name}} aalu</p>
+          @endif
+          <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class='	far fa-user-circle' style='font-size:30px margin-right=1px' href="#"></i>
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end">
+            @if(!auth()->check())
+            <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
+            @else
+            <!-- <li><a class="dropdown-item" href="{{route('login')}}">{{auth()->user()->name}}</a></li> -->
+            <li><button class="btn btn-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">View Booking</button>
+
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                <div class="offcanvas-header">
+                  <h5 id="offcanvasRightLabel">View Booking</h5>
+                  <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                  <!-- <table id="example" class="display  ">
+                    <thead>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Check_in</th>
+                      <th>Check_out</th>
+                      <th>Room </th>
+                      <th>Payment Status</th>
+                      <th>Action</th>
+                    </thead>
+                    <tbody>
+                      @foreach($forms as $form)
+
+                      <tr>
+                        <td>{{$form->name}}</td>
+                        <td>{{$form->email}}</td>
+                        <td>{{$form->check_in}}</td>
+                        <td>{{$form->check_out}}</td>
+                        <td>{{$form->room->room_type}}</td>
+                        <td><span class="bg-yellow-400 text-white text-m font-medium mr-2 px-2.5 py-1 rounded dark:bg-yellow-400">Pending</span>
+
+                        </td>
+                        <td>
+                          <a href="" class="bg-blue-600 text-white px-4 py-1 rounded-lg mx-1">Edit</a>
+                          <a onclick="showDelete" class="bg-red-600 text-white px-4 py-1 rounded-lg mx-1 cursor-pointer">Delete</a>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table> -->
+
+                </div>
+              </div>
+            </li>
+            <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
+            @endif
+            <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</nav> 
-   </header>
+  </nav>
+</header>

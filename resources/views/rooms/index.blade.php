dee7a9d6aad419bf7d5ffa656297800a6bf63899
PravinGhimire <ghimireprabin62@gmail.com>
2023-05-17 11:50:05 +0545

    nnn

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -3,9 +3,8 @@
 @include('layouts.message')
 
 <h2 class="text-4xl font-bold text-red-500 ">Rooms</h2>
-<div class="my-4 text-right">
-    <a href="{{route('rooms.create')}}" class="bg-green-500 text-white rounded-lg px-3 py-2">Add Room</a>
-
+<div class="my-2 text-right">
+    <a href="{{route('rooms.create')}}" class="bg-green-500 text-white rounded-lg px-2 py-2">Add Room</a>
 </div>
 <table id="example" class="display">
     <thead>
